600956673cd6ab1bdcd14488b8af2c31c36475d1
Chiiicken <chiiicken5538@gmail.com>
2022-09-26 18:41:52 +0200

    Update cxcontrol.lua

--- cxcontrol.lua
+++ cxcontrol.lua
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> de7f52e0319882e01a011b9deb2a87e9c339a052
 getgenv().Settings = {
     host = 2725017616,
     fps = 10,
@@ -25,56 +21,21 @@ getgenv().Alts = {
 	Alt8 = 3211518838, -- BlushedDHC_8
 	Alt9 = 3211520385, -- BlushedDHC_9
 	Alt10 = 3319206334, -- BlushedDHC_10
-<<<<<<< HEAD
-=======
     Alt11 = 2725017616
->>>>>>> de7f52e0319882e01a011b9deb2a87e9c339a052
 }
 
 -----------
 
-<<<<<<< HEAD
-local cxlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/cicerilisu/cA6rvMoKjjt1433txjPz/main/cxlib.lua"))()
-=======
 local cxlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/cicerilisu/cA6rvMoKjjt1433txjPz/main/cxlib.lua"))
->>>>>>> de7f52e0319882e01a011b9deb2a87e9c339a052
 local isHost = false
 
 local LoadedPackages = {}
 local LoadedFunctions = {}
-<<<<<<< HEAD
-local alts = 0
-=======
-
->>>>>>> de7f52e0319882e01a011b9deb2a87e9c339a052
 
 if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
-<<<<<<< HEAD
-for k, v in pairs(getgenv().Alts) do
-    alts = alts + 1
-end
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/cicerilisu/cA6rvMoKjjt1433txjPz/main/extras/AnticheatDisabler.lua"))()
-
-        
-if game:GetService("Players").LocalPlayer.UserId == getgenv().Settings.host then
-    isHost = true
-    print("This is host")
-    cxlib:sendMessageRequest("This is host.")
-else
-    for i = 1, alts, 1 do
-        if game:GetService("Players").LocalPlayer.UserId == getgenv().Alts["Alt" .. i] then
-            
-            print("[cxcontrol] This is an alt.")
-            print("[cxcontrol] ^^ Alt" .. i .. "^^")
-            
-            cxlib:sendMessageRequest("cx v1.0 BETA")
-        end
-    end
-=======
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/cicerilisu/cA6rvMoKjjt1433txjPz/main/extras/AnticheatDisabler.lua"))
 
@@ -96,7 +57,6 @@ else
     end
     print("[cxcontrol] user is not grouped.")
     return
->>>>>>> de7f52e0319882e01a011b9deb2a87e9c339a052
 end
 
 -- Check if package is already loaded // Load package
@@ -126,13 +86,8 @@ function handleCommand(rawMessage)
     if isHost then return end
     if tostring(rawMessage:lower()):find(getgenv().Settings.prefix:lower()) and not tostring(rawMessage:lower()):find(" ") then
         message = rawMessage:lower():gsub(getgenv().Settings.prefix, "")
-<<<<<<< HEAD
-        print("!!!!!!!!", message)
-        if LoadPackage(message) ~= "package already initialized" then
-=======
         
         if LoadedPackages(message) ~= "package already initialized" then
->>>>>>> de7f52e0319882e01a011b9deb2a87e9c339a052
             LoadedFunctions[message]()
         end
     else
@@ -149,18 +104,9 @@ function handleCommand(rawMessage)
     end
 end
 
-<<<<<<< HEAD
--- Receive messages
-
-game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.OnMessageDoneFiltering.OnClientEvent:Connect(function(data) 
-    print("[cx] [debug] Received raw chat message: " .. data.FromSpeaker .. " >> " .. data.Message)
-    handleCommand(data.Message)
-end)
-=======
 -- Receive messages 
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.OnMessageDoneFiltering.OnMessageDoneFiltering:Connect(function(inputData)
     if game:GetService("Players"):FindFirstChild(inputData.FromSpeaker).UserId == getgenv().Settings.host then
         handleCommand(data.Message)
     end
 end)
->>>>>>> de7f52e0319882e01a011b9deb2a87e9c339a052
