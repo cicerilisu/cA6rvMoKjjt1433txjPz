02eb5aeb495ec0b670f981611949fae2d359efea
cicerilisu <114285825+cicerilisu@users.noreply.github.com>
2022-09-29 22:10:33 +0200

    Update cxlib.lua

--- cxlib.lua
+++ cxlib.lua
@@ -2,6 +2,23 @@ print("cxlib loaded")
 
 local cxlib = {}
 
+local animationtable = {
+    Enum.HumanoidStateType.FallingDown,
+    Enum.HumanoidStateType.Running,
+    Enum.HumanoidStateType.RunningNoPhysics,
+    Enum.HumanoidStateType.Climbing,
+    Enum.HumanoidStateType.StrafingNoPhysics,
+    Enum.HumanoidStateType.Ragdoll,
+    Enum.HumanoidStateType.GettingUp,
+    Enum.HumanoidStateType.Jumping,
+    Enum.HumanoidStateType.Landed,
+    Enum.HumanoidStateType.Flying,
+    Enum.HumanoidStateType.Freefall,
+    Enum.HumanoidStateType.PlatformStanding,
+    Enum.HumanoidStateType.Dead,
+    Enum.HumanoidStateType.Swimming,
+}
+
 function commandArguments(...)
     local one = {}
     for k, v in pairs(...) do
@@ -17,5 +34,14 @@ function cxlib:sendMessageRequest(message)
     game:GetService("ReplicatedStorage"):FindFirstChild("DefaultChatSystemChatEvents")
         .SayMessageRequest:FireServer(message, "All")
 end
+
+function cxlib:disableAnimations()
+    for k, v in pairs(animationtable) do
+        game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid"):SetStateEnabled(v, false)
+    end
+end
+
+
+
 
 return cxlib
