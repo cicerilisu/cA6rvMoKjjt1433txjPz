75e4d3eb8ea871073d1f1380199bb65ad968d50c
cicerilisu <114285825+cicerilisu@users.noreply.github.com>
2022-09-30 10:18:26 +0200

    Update fpsCommand.lua

--- commands/fpsCommand.lua
+++ commands/fpsCommand.lua
@@ -1,6 +1,18 @@
 return function (...)
-    print("FPS Command executed!")
     args = commandArguments(...)
-    setfpscap(tonumber(args[2]))
-    cxlib:sendMessageRequest("FPS set to " .. args[2])
+
+    if args[2] == nil then        
+        print("[cxcontrol] Failed to set FPS value (FPS value missing)")
+        if game:GetService("Players").LocalPlayer.UserId == getgenv().Alts.Alt1 then
+            cxlib:sendMessageRequest("Please provide a FPS value!") 
+        end
+    elseif tonumber(args[2]) == nil then
+        print("[cxcontrol] Failed to set FPS value (FPS value not numeric/nil)")
+        if game:GetService("Players").LocalPlayer.UserId == getgenv().Alts.Alt1 then
+            cxlib:sendMessageRequest("Invalid fps value")
+        end
+    elseif not crashing then
+        setfpscap(tonumber(args[2]))
+        sendMessage("Fps set to " .. tostring(args[2]))
+    end
 end
