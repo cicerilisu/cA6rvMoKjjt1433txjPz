333c8db58b1a36b83436f8f02f1dc56c03403f46
cicerilisu <114285825+cicerilisu@users.noreply.github.com>
2022-11-17 19:52:25 +0100

    Update altsCommand.lua

--- commands/altsCommand.lua
+++ commands/altsCommand.lua
@@ -12,25 +12,16 @@ return function(...)
     if args[2] == "spot" then
         print("Executed alts command with option 'spot'")
 
-        if game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Anchored then
-            print("Not unlocked! unlocking...")
-            game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Anchored = false
-        end
-
+        print("aa")
+        
+        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Anchored = false
         
         temp_x = game:GetService("Players"):GetPlayerByUserId(getgenv().Settings.host).Character.HumanoidRootPart.Position.X
         temp_y = game:GetService("Players"):GetPlayerByUserId(getgenv().Settings.host).Character.HumanoidRootPart.Position.Y
         temp_z = game:GetService("Players"):GetPlayerByUserId(getgenv().Settings.host).Character.HumanoidRootPart.Position.Z + 10
         
-        repeat 
-            game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(temp_x, temp_y, temp_z)   
-            task.wait()
-        until (game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position - Vector3.new(temp_x, temp_y, temp_z)).Magnitude <= 1
-
-        if not game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Anchored then
-            print("Not locked! locking...")
-            game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Anchored = true
-        end
+        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(temp_x, temp_y, temp_z)   
+        
     elseif args[2] == "line" then
             
         print("no")
